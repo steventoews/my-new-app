087f90f760037e956bb5df6d98dbf8d52f302f31
steventoews <61131568+steventoews@users.noreply.github.com>
2024-02-16 15:55:56 -0400

    first commit

---
+++ app/files/[file]/page.tsx
@@ -0,0 +1,11 @@
+export default function File() {
+    return (
+      <main>
+        <p>
+          File
+        </p>
+  
+      </main>
+    );
+  }
+  
---
+++ app/files/page.tsx
@@ -0,0 +1,11 @@
+export default function Files() {
+    return (
+      <main>
+        <p>
+          Files
+        </p>
+  
+      </main>
+    );
+  }
+  
